cef35ed37ad074be41553fa4090ed99a653aa960
Vladimir Alfaro <vladimir.ac29@gmail.com>
2025-07-27 12:34:02 -0400

    put widgets together into main page

--- lib/src/features/profile/presentation/pages/profile_page.dart
+++ lib/src/features/profile/presentation/pages/profile_page.dart
@@ -1,4 +1,6 @@
 // lib/src/features/profile/presentation/pages/profile_page.dart
+import 'package:celebreak_profile_page/src/features/profile/presentation/widgets/profile_header.dart';
+import 'package:celebreak_profile_page/src/features/profile/presentation/widgets/user_info_section.dart';
 import 'package:flutter/material.dart';
 
 class ProfilePage extends StatelessWidget {
@@ -11,9 +13,10 @@ class ProfilePage extends StatelessWidget {
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            // 1. ProfileHeader
+            ProfileHeader(key: Key('profile_header')),
             SizedBox(height: 24),
-            // 2. UserInfoSection
+            // ---- Scrolleable part ----
+            UserInfoSection(key: Key('user_info')),
             SizedBox(height: 24),
             // 3. InterestsSection
             SizedBox(height: 24),
