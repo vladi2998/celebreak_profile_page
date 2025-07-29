1f1e5923835704867cd639b45446c576ca044286
Vladimir Alfaro <vladimir.ac29@gmail.com>
2025-07-28 22:42:44 -0400

    feat: first approach for user info section into user_info_section widget

--- lib/src/features/profile/presentation/widgets/user_info_section.dart
+++ lib/src/features/profile/presentation/widgets/user_info_section.dart
@@ -1,6 +1,7 @@
 // lib/src/features/profile/presentation/widgets/user_info_section.dart
 import 'package:flutter/material.dart';
 import 'package:eva_icons_flutter/eva_icons_flutter.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 
 class UserInfoSection extends StatelessWidget {
   const UserInfoSection({super.key});
@@ -9,6 +10,13 @@ class UserInfoSection extends StatelessWidget {
   Widget build(BuildContext context) {
     final screenWidth = MediaQuery.of(context).size.width;
     const double horizontalPaddingRatio = 0.9;
+    const Map userInfoMockup = {
+      'job': {'role': 'Event Planner', 'company': 'Celebreak'},
+      'location': ['Buenos Aires', 'Franfurt', 'Madrid'],
+      'language': ['English', 'Spanish', 'Catalan'],
+      'teams': ['Boca Juniors', 'FC Barcelona'],
+      'idols': ['Patrick Viera', 'Sergio Busquets', 'Lionel Messi'],
+    };
 
     return Column(
       crossAxisAlignment: CrossAxisAlignment.center,
@@ -40,7 +48,7 @@ class UserInfoSection extends StatelessWidget {
             ),
           ),
         ),
-        const SizedBox(height: 20),
+        const SizedBox(height: 25),
         // 2. Bio Text
         Padding(
           padding: EdgeInsets.symmetric(
@@ -59,11 +67,11 @@ class UserInfoSection extends StatelessWidget {
             ),
           ),
         ),
-        const SizedBox(height: 20),
+        const SizedBox(height: 25),
         // 3. "kudos received" Section
         Container(
           width: screenWidth * horizontalPaddingRatio,
-          padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 12),
+          padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 17),
           decoration: BoxDecoration(
             color: const Color.fromRGBO(21, 21, 21, 1),
             borderRadius: BorderRadius.circular(8),
@@ -80,6 +88,151 @@ class UserInfoSection extends StatelessWidget {
                   fontSize: 14,
                   fontWeight: FontWeight.w500,
                 ),
+              ),
+            ],
+          ),
+        ),
+        // 4. personal details section
+        Container(
+          width: screenWidth * horizontalPaddingRatio,
+          padding: const EdgeInsets.symmetric(horizontal: 5, vertical: 10),
+          child: Column(
+            children: [
+              // work
+              SizedBox(height: 20),
+              Row(
+                children: [
+                  SvgPicture.asset('assets/images/svg/job.svg'),
+                  SizedBox(width: 8),
+                  Text(
+                    userInfoMockup['job']['role'],
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 17,
+                      fontWeight: FontWeight.w400,
+                    ),
+                  ),
+                  Text(
+                    ' at ',
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 17,
+                      fontWeight: FontWeight.w100,
+                    ),
+                  ),
+                  Text(
+                    userInfoMockup['job']['company'],
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 17,
+                      fontWeight: FontWeight.w400,
+                    ),
+                  ),
+                ],
+              ),
+              // location
+              SizedBox(height: 20),
+              Row(
+                children: [
+                  SvgPicture.asset('assets/images/svg/location.svg'),
+                  SizedBox(width: 8),
+                  Text(
+                    'Lived in ',
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 17,
+                      fontWeight: FontWeight.w100,
+                    ),
+                  ),
+                  ...userInfoMockup['location'].map((location) {
+                    return Text(
+                      '$location, ',
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 17,
+                        fontWeight: FontWeight.w400,
+                      ),
+                    );
+                  }).toList(),
+                ],
+              ),
+              // languages
+              SizedBox(height: 20),
+              Row(
+                children: [
+                  SvgPicture.asset('assets/images/svg/languages.svg'),
+                  SizedBox(width: 8),
+                  Text(
+                    'Speaks ',
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 17,
+                      fontWeight: FontWeight.w100,
+                    ),
+                  ),
+                  ...userInfoMockup['language'].map((language) {
+                    return Text(
+                      '$language, ',
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 17,
+                        fontWeight: FontWeight.w400,
+                      ),
+                    );
+                  }).toList(),
+                ],
+              ),
+              // teams
+              SizedBox(height: 20),
+              Row(
+                children: [
+                  SvgPicture.asset('assets/images/svg/teams.svg'),
+                  SizedBox(width: 8),
+                  Text(
+                    'Supports ',
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 17,
+                      fontWeight: FontWeight.w100,
+                    ),
+                  ),
+                  ...userInfoMockup['teams'].map((team) {
+                    return Text(
+                      '$team, ',
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 17,
+                        fontWeight: FontWeight.w400,
+                      ),
+                    );
+                  }).toList(),
+                ],
+              ),
+              // idols
+              SizedBox(height: 20),
+              Row(
+                children: [
+                  SvgPicture.asset('assets/images/svg/player_idols.svg'),
+                  SizedBox(width: 8),
+                  Text(
+                    'Admires ',
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 17,
+                      fontWeight: FontWeight.w100,
+                    ),
+                  ),
+                  ...userInfoMockup['idols'].map((idol) {
+                    return Text(
+                      '$idol, ',
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 17,
+                        fontWeight: FontWeight.w400,
+                      ),
+                    );
+                  }).toList(),
+                ],
               ),
             ],
           ),
