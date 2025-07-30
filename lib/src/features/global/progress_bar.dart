b40d9a3621eca6f004e89b80324b1e7fdb7853ef
Vladimir Alfaro <vladimir.ac29@gmail.com>
2025-07-29 22:04:07 -0400

    feat: create new progress bar global widget to be used into football skills section

---
+++ lib/src/features/global/progress_bar.dart
@@ -0,0 +1,76 @@
+import 'package:flutter/material.dart';
+
+class ProgressBar extends StatelessWidget {
+  const ProgressBar({required this.skill, required this.level, super.key});
+
+  final String skill;
+  final double level;
+  @override
+  Widget build(BuildContext context) {
+    // Ensure level is clamped between 0.0 and 5
+    final clampedLevel = level.clamp(0.0, 1);
+
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Text(
+              skill,
+              style: const TextStyle(
+                color: Colors.white,
+                fontSize: 16,
+                fontWeight: FontWeight.w500,
+              ),
+            ),
+            Text(
+              "${(clampedLevel * 5).round()}/5",
+              style: const TextStyle(
+                color: Colors.white,
+                fontSize: 16,
+                fontWeight: FontWeight.w500,
+              ),
+            ),
+          ],
+        ),
+        const SizedBox(height: 8),
+        Stack(
+          children: [
+            // Background of the progress bar
+            Container(
+              height: 8.0,
+              decoration: BoxDecoration(
+                color: const Color.fromRGBO(
+                  50,
+                  50,
+                  50,
+                  1,
+                ), // Darker gray for background
+                borderRadius: BorderRadius.circular(5.0),
+              ),
+            ),
+            // Foreground (filled part) of the progress bar
+            LayoutBuilder(
+              builder: (context, constraints) {
+                return Container(
+                  height: 8.0,
+                  width: constraints.maxWidth * clampedLevel,
+                  decoration: BoxDecoration(
+                    color: const Color.fromRGBO(
+                      255,
+                      185,
+                      0,
+                      1,
+                    ), // Yellow color for progress
+                    borderRadius: BorderRadius.circular(5.0),
+                  ),
+                );
+              },
+            ),
+          ],
+        ),
+      ],
+    );
+  }
+}
